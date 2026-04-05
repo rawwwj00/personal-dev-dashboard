2b653b65367119f2c7759cb12ed08a28428f83dc
RAJ SACHAN <raj.24bce10022@vitbhopal.ac.in>
2026-04-05 22:00:01 +0530

    Fixed Cloudinary fetch issues

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,5 +1,5 @@
 const { v2: cloudinary } = require('cloudinary');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const CloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
 
 cloudinary.config({
